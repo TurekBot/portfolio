a3c9d3605ba45a665d7c1f02745ede5aec2febe2
Bradley Turek <bturek@asu.edu>
2021-11-05 15:09:10 -0600

    feat: add man svg; allow style via css
    I was able to get the man in, but it's not behaving very well. I got everything filled that needed to be filled and left just the shirt unfilled.

--- pages/index.js
+++ pages/index.js
@@ -16,70 +16,11 @@ export default function Home() {
       <main>
         <h1 className="title">Man of Many Hats</h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porta
-          ante justo, at efficitur enim efficitur ut. Fusce luctus turpis
-          sapien, sit amet consequat nisi ultricies nec. Sed sed viverra sem.
-          Nam tincidunt, sapien sed vulputate gravida, est velit faucibus sem,
-          et suscipit sem massa et urna. Cras pharetra metus sit amet maximus
-          mollis. Nulla sed sollicitudin velit, at bibendum mi. Integer et
-          semper velit. Duis dignissim ante tortor, at luctus odio tristique
-          vel. Pellentesque viverra nibh nec libero eleifend ultricies.
-          Phasellus lobortis arcu et est eleifend faucibus. Phasellus nec
-          gravida risus. Suspendisse justo leo, finibus vel maximus sed, dapibus
-          ut metus. Orci varius natoque penatibus et magnis dis parturient
-          montes, nascetur ridiculus mus. Mauris aliquam semper dolor et
-          feugiat. Sed tincidunt vehicula varius.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porta
-          ante justo, at efficitur enim efficitur ut. Fusce luctus turpis
-          sapien, sit amet consequat nisi ultricies nec. Sed sed viverra sem.
-          Nam tincidunt, sapien sed vulputate gravida, est velit faucibus sem,
-          et suscipit sem massa et urna. Cras pharetra metus sit amet maximus
-          mollis. Nulla sed sollicitudin velit, at bibendum mi. Integer et
-          semper velit. Duis dignissim ante tortor, at luctus odio tristique
-          vel. Pellentesque viverra nibh nec libero eleifend ultricies.
-          Phasellus lobortis arcu et est eleifend faucibus. Phasellus nec
-          gravida risus. Suspendisse justo leo, finibus vel maximus sed, dapibus
-          ut metus. Orci varius natoque penatibus et magnis dis parturient
-          montes, nascetur ridiculus mus. Mauris aliquam semper dolor et
-          feugiat. Sed tincidunt vehicula varius.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porta
-          ante justo, at efficitur enim efficitur ut. Fusce luctus turpis
-          sapien, sit amet consequat nisi ultricies nec. Sed sed viverra sem.
-          Nam tincidunt, sapien sed vulputate gravida, est velit faucibus sem,
-          et suscipit sem massa et urna. Cras pharetra metus sit amet maximus
-          mollis. Nulla sed sollicitudin velit, at bibendum mi. Integer et
-          semper velit. Duis dignissim ante tortor, at luctus odio tristique
-          vel. Pellentesque viverra nibh nec libero eleifend ultricies.
-          Phasellus lobortis arcu et est eleifend faucibus. Phasellus nec
-          gravida risus. Suspendisse justo leo, finibus vel maximus sed, dapibus
-          ut metus. Orci varius natoque penatibus et magnis dis parturient
-          montes, nascetur ridiculus mus. Mauris aliquam semper dolor et
-          feugiat. Sed tincidunt vehicula varius.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porta
-          ante justo, at efficitur enim efficitur ut. Fusce luctus turpis
-          sapien, sit amet consequat nisi ultricies nec. Sed sed viverra sem.
-          Nam tincidunt, sapien sed vulputate gravida, est velit faucibus sem,
-          et suscipit sem massa et urna. Cras pharetra metus sit amet maximus
-          mollis. Nulla sed sollicitudin velit, at bibendum mi. Integer et
-          semper velit. Duis dignissim ante tortor, at luctus odio tristique
-          vel. Pellentesque viverra nibh nec libero eleifend ultricies.
-          Phasellus lobortis arcu et est eleifend faucibus. Phasellus nec
-          gravida risus. Suspendisse justo leo, finibus vel maximus sed, dapibus
-          ut metus. Orci varius natoque penatibus et magnis dis parturient
-          montes, nascetur ridiculus mus. Mauris aliquam semper dolor et
-          feugiat. Sed tincidunt vehicula varius.
-        </p>
+        <section className="software-developer">
+          <svg id="avatar">
+            <use href="/Man.svg#Man"></use>
+          </svg>
+        </section>
       </main>
 
       <Footer />
