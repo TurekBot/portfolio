71b6528c30aead2dfa5afc55435c4433368ffdec
Bradley Turek <bturek@asu.edu>
2021-11-08 09:51:46 -0700

    feat: :lipstick: add accented rollover to each nav item
    When you mouseover the navigation item, it changes to it's accent color.

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,19 +1,54 @@
-
+import { convertRoleNametoClassName } from "./RoleCard";
 
 export default function Navigation() {
-    return (
-      <section className="left-bar">
-        <a href="/"><span className="subtitle">Brad Turek</span></a>
-        {/* <span className="subliminal-messaging">(He's your guy)</span> */}
-        <nav>
-          <ul>
+  return (
+    <section className="left-bar">
+      <a href="/">
+        <span className="subtitle">Brad Turek</span>
+      </a>
+      {/* <span className="subliminal-messaging">(He's your guy)</span> */}
+      <nav>
+        <ul>
           {/* <li><a href="/">Home</a></li> */}
-          <li><a href="the-software-developer">Software Developer</a></li>
-          <li><a href="the-visual-communicator">Visual Communicator</a></li>
-          <li><a href="the-ux-designer">UX Designer</a></li>
-          <li><a href="the-technical-writer-editor">Technical Writer & Editor</a></li>
-          <li><a href="#">Contact</a></li>
-          </ul>
-        </nav>
-      </section>);
-  }
+          <li>
+            <a
+              href="the-software-developer"
+              className={convertRoleNametoClassName("Software Developer")}
+            >
+              Software Developer
+            </a>
+          </li>
+          <li>
+            <a
+              href="the-visual-communicator"
+              className={convertRoleNametoClassName("Visual Communicator")}
+            >
+              Visual Communicator
+            </a>
+          </li>
+          <li>
+            <a
+              href="the-ux-designer"
+              className={convertRoleNametoClassName("UX Designer")}
+            >
+              UX Designer
+            </a>
+          </li>
+          <li>
+            <a
+              href="the-technical-writer-editor"
+              className={convertRoleNametoClassName(
+                "Technical Writer & Editor"
+              )}
+            >
+              Technical Writer & Editor
+            </a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
+}
