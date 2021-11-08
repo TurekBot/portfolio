d3472ffe396199b63e557717156dcc6242e6512b
Bradley Turek <bturek@asu.edu>
2021-11-08 12:29:41 -0700

    feat: :heavy_plus_sign: add pages for each role
    Before there was no pages for the last roles; now we've got a page for each role.

--- pages/the-ux-designer.js
+++ pages/the-ux-designer.js
@@ -4,16 +4,16 @@ import HeadStuff from "../components/HeadStuff";
 import Navigation from "../components/Navigation";
 import RolePage from "../components/RolePage";
 
-export default function TheSoftwareDeveloper() {
+export default function TheUxDesigner() {
   return (
     <>
       <Head>
-        <title>Brad the Software Developer</title>
+        <title>Brad the UX Designer</title>
         <HeadStuff />
       </Head>
       <Navigation />
       <RolePage
-        pageTitle="Software Developer"
+        pageTitle="UX Designer"
         intro="Sed ac enim mollis, vulputate turpis sit amet, sollicitudin eros. Cras justo nunc, consequat sed imperdiet vel, molestie ut ex. Aenean sit amet mi id arcu varius elementum. Quisque dignissim purus quis erat lacinia, et vulputate quam tempor. Duis consectetur quam sed quam hendrerit egestas. Aliquam dictum urna fringilla arcu mattis rhoncus."
       ></RolePage>
       <Footer/>
--- pages/the-visual-communicator.js
+++ pages/the-visual-communicator.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Footer from "../components/Footer";
 import HeadStuff from "../components/HeadStuff";
 import Navigation from "../components/Navigation";
 import RolePage from "../components/RolePage";
@@ -15,6 +16,7 @@ export default function TheVisualCommunicator() {
         pageTitle="Visual Communicator"
         intro="Sed ac enim mollis, vulputate turpis sit amet, sollicitudin eros. Cras justo nunc, consequat sed imperdiet vel, molestie ut ex. Aenean sit amet mi id arcu varius elementum. Quisque dignissim purus quis erat lacinia, et vulputate quam tempor. Duis consectetur quam sed quam hendrerit egestas. Aliquam dictum urna fringilla arcu mattis rhoncus."
       ></RolePage>
+      <Footer/>
     </>
   );
 }
