6df9ad61ba39846cc5fea149f50853f69ff229f5
Bradley Turek <bturek@asu.edu>
2021-11-15 19:38:39 -0700

    perf: add link to artifact card image
    I also simplified how the link is passed to the image and button

--- components/ArtifactCard.js
+++ components/ArtifactCard.js
@@ -1,26 +1,31 @@
 import { convertRoleNameToPageName } from "./RoleCard";
 
 export default function ArtifactCard(props) {
-  const linkHref = convertRoleNameToPageName(props.parent) + "/" + props.id;
+  const linkHref =
+    props.id && props.parent
+      ? convertRoleNameToPageName(props.parent) + "/" + props.id
+      : "#";
 
   return (
-    
-      <figure className={"artifact-card " + props.id}>
-        {props.thumbnail ? <img src={props.thumbnail} /> : placeholder}
-        <figcaption>
-          <h1>{props.artifactTitle}</h1>
-          <p>{props.hook}</p>
-        </figcaption>
-        <a href={props.id && props.parent ? linkHref : "#"} className="see-more-button">See more...</a>
-        <style jsx>{`
-          figure {
-            text-align: center;
-          }
-          h1 {
-            margin: 0;
-          }
-        `}</style>
-      </figure>
-
+    <figure className={"artifact-card " + props.id}>
+      <a href={linkHref}>
+        <img src={props.thumbnail} />
+      </a>
+      <figcaption>
+        <h1>{props.artifactTitle}</h1>
+        <p>{props.hook}</p>
+      </figcaption>
+      <a href={linkHref} className="see-more-button">
+        See more...
+      </a>
+      <style jsx>{`
+        figure {
+          text-align: center;
+        }
+        h1 {
+          margin: 0;
+        }
+      `}</style>
+    </figure>
   );
 }
